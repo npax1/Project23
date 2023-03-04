a4424ea30b7de95c5d80d56e9b7d849fda2c1633
bob <akela2018a@gmail.com>
2023-03-04 10:54:10 +0200

    Add project files.

---
+++ Header.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+int mystrlen(const char* str);
+char* mysrtcpy(char* str1, const char* str2);
+char* mystrcat(char* str1, const char* str2);
+char* mystrchr(char* str, char s);
+char* mystrstr(char* str1, char* str2);
